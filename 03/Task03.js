9f60d325cf4038f23e2fafe88e4328c3ef1b807a
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-10-26 21:00:51 +0200

    add task 04

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, useParams, Link } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
 import Shop from "../src/components/Shop";
 import products from "./../src/products.json";
 
--- 04/Task04.js
+++ 04/Task04.js
@@ -1,10 +1,36 @@
-import React from 'react';
-
+import React from "react";
+import { Route, useHistory } from "react-router-dom";
+import Shop from "../src/components/Shop";
+import products from "../src/products.json";
 const Task04 = () => {
-    return (
-        <h1>Task04</h1>
-    );
-}
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    history.push(event.target.value);
+  };
+  return (
+    <>
+      <h1>Task04</h1>
+
+      <label for="options">Select a filter:</label>
+      <select onChange={(e) => handleChange(e)}>
+        <option value="/task04/price-desc">Products descending</option>
+
+        <option value="/task04/price-asc">Products ascending</option>
+        <option value="/task04/price-name">Products name</option>
+      </select>
+
+      <Route path="/task04/price-desc">
+        <Shop list={[...products].sort((a, b) => b.price - a.price)} />
+      </Route>
+      <Route path="/task04/price-asc">
+        <Shop list={[...products].sort((a, b) => a.price - b.price)} />
+      </Route>
+      <Route path="/task04/price-name">
+        <Shop list={[...products].sort((a, b) => a.name - b.name)} />
+      </Route>
+    </>
+  );
+};
 
 export default Task04;
-
